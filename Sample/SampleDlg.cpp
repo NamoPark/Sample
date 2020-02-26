d90990beb8a8e44f779242b674c6e8dbda32fe8e
nhpark <nhpark@DESKTOP-F13BD6F>
2020-02-26 09:56:49 +0900

    UDP_Streaming

--- Sample/SampleDlg.cpp
+++ Sample/SampleDlg.cpp
@@ -177,7 +177,6 @@ BOOL CSampleDlg::OnInitDialog()
 		strMsg.Format(_T("Failed to enter ABOVE_NORMAL_PRIORITY_CLASS mode (%d)"), dwError);
 		AfxMessageBox(strMsg);
 	}
-
 	return true;  // 포커스를 컨트롤에 설정하지 않으면 true를 반환합니다.
 }
 
